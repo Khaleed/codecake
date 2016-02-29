7faae149c2171ebb8fc96e4c0eb0f298e36fbe96
Khalid Omar Ali <khalid.ali@consultant.com>
2016-02-29 03:59:07 +0000

    add Chai Mocha test file

---
+++ test/tree.test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+
+describe('Tree', function() {
+    describe('#Node', function () {
+        it('should return nodes in a hierarchical relationship', function () {
+            // assertions
+        });
+    });
+});
