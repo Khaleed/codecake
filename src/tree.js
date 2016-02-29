ccb61d6235a8af540c73a54b427298428f4ee7fa
Khalid Omar Ali <khalid.ali@consultant.com>
2016-02-29 00:44:57 +0000

    implement a basic tree and test it

--- src/tree.js
+++ src/tree.js
@@ -1,21 +1,24 @@
-/* Implement a tree data structure
-   start with a function that creates a node
-   each node holds data and has a parent or children
- */
+"use strict";
 
-let Node = data => {
+/* Implement a tree data structure start with a function that creates a node each node holds data and has a parent or children */
+
+function Node(data) {
     this.data = data;
     this.parent = null;
     this.children = [];
 };
 
 /* function that makes a tree
-   start with a new instance of Node
-   assign the Node as the root Node
+ start with a new instance of Node
+ assign the Node as the root Node
  */
-let Tree = data => {
+
+console.log(new Node('CEO'));
+function Tree(data)  {
     let node = new Node(data);
     this.root = node;
 };
 
 // test
+let CEO = new Tree("CEO");
+console.log(CEO);
