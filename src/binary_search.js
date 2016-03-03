7a93d361e44f317ea275a07bc27eb602530e22a8
Khalid Omar Ali <khalid.ali@consultant.com>
2016-03-03 03:12:56 +0000

    add high-level description

---
+++ src/binary_search.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// Implement Binary Search Algorithm
+
+binarySearch = (key, list, min, max) => {
+
+    // base case is that key is not even in the list
+    if (max < min) {
+        return -1;
+    }
+    // find a midpoint
+
+    // check if value at the midpoint is > or < key that we are looking for
+
+    // if it is < key, this means that we can ignore whatever is on the left side of midpoint value & Update min and max
+
+    // if it is > key, this means we can ignore whatever is on the right side of the midpoint value & update min & max
+
+    // else the midpoint value === key then we simply return the midpoint
+
+};
