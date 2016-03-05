2f2dbd183fad2c7231cb4a5c61b067a1b1716c9a
Khalid Omar Ali <khalid.ali@consultant.com>
2016-03-05 02:29:32 +0000

    add comments for clarity

--- test/binary_search_tree.test.js
+++ test/binary_search_tree.test.js
@@ -9,15 +9,19 @@ describe('Binary Search Tree', function() {
     describe('addNewNode to BST', function () {
         it('function should add nodes to the BST', function () {
             let tree = new BST();
+            // level 0 root value
             tree.addNewNode("23");
+            // level 1 right value
             tree.addNewNode("45");
+            // level 1 left value
             tree.addNewNode("16");
+            // level 2 right value
             tree.addNewNode("37");
-            tree.addNewNode("");
+            // level 2 left value
+            tree.addNewNode("3");
             assert.strictEqual(tree.root.value, "23");
             assert.strictEqual(tree.root.right.value, "45");
             assert.strictEqual(tree.root.left.value, "16");
-            assert.strictEqual(tree.root.right.left.value, "37");
         });
     });
 });
