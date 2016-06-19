e1ff4d3440345578e178599b797da607017755de
Khalid Omar Ali <khalid.ali@consultant.com>
2016-06-19 20:07:43 +0100

    summarise what a linked list is

---
+++ src/linkedlist.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**Array is one large contiguous block of memory
+   you get random access in constant time O(1)
+   for a linked list, data is stored in non-contigous block of memory
+   each node has two parts, one to store the data and the other to store a pointer to the next data
+   and access and insertion time is O(n)
+   we can create and delete nodes as when we want
+   you don't have to guess the size of the list beforehand
+ **/
