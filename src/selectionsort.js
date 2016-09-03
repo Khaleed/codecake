c92062a5be18e6273e5aa1f08afa0ae07bda7e8f
Khalid Omar Ali <khalid.ali@consultant.com>
2016-09-03 03:50:46 +0100

    change syntax to ES2015

--- src/selectionsort.js
+++ src/selectionsort.js
@@ -1,15 +1,15 @@
-function selectionSort(unsorted) {
-    var sortedList = [];
-    var origLength = unsorted.length;
-    for (var i = 0; i < origLength; i += 1) {
+const selectionSort = unsorted => {
+    let sortedList = [];
+    let origLength = unsorted.length;
+    for (let i = 0; i < origLength; i += 1) {
         // find smallest
-        var smallestVal = unsorted[0];
-        var smallestIndex = 0;
-        for (var j = 1; j < unsorted.length; j+= 1) {
+        let smallestVal = unsorted[0];
+        let smallestIndex = 0;
+        for (let i = 1; i < unsorted.length; i += 1) {
             // check if current elem is smaller than smallestVal
-            if (unsorted[j] < smallestVal) {
-                smallestVal = unsorted[j];
-                smallestIndex = j;
+            if (unsorted[i] < smallestVal) {
+                smallestVal = unsorted[i];
+                smallestIndex = i;
             }
         }
         // push smallest to sortedList
@@ -18,13 +18,13 @@ function selectionSort(unsorted) {
         unsorted.splice(smallestIndex, 1);
     }
     return sortedList;
-}
+};
 
-var list1 = [14, 13, 12, 10, 9];
+let list1 = [14, 13, 12, 10, 9];
 
 console.log(selectionSort(list1));
 
-var list2 = [5, 95, 60, 55, 29, 75, 27,	18,	56, 5, 73, 29,	79,	36,	34,
+let list2 = [5, 95, 60, 55, 29, 75, 27,	18,	56, 5, 73, 29,	79,	36,	34,
              1,	87, 57, 58,	32];
 
 console.log(selectionSort(list2));
