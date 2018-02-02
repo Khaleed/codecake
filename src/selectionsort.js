df21606e2462c709dfd00b7f579c07e246a7d40e
Khalid <khalid@alum.recurse.com>
2018-02-02 01:02:26 +0000

    Re-implement selection sort

--- src/selectionsort.js
+++ src/selectionsort.js
@@ -1,30 +1,49 @@
-const selectionSort = unsorted => {
-    let sortedList = [];
-    let origLength = unsorted.length;
-    for (let i = 0; i < origLength; i += 1) {
-        // find smallest
-        let smallestVal = unsorted[0];
-        let smallestIndex = 0;
-        for (let i = 1; i < unsorted.length; i += 1) {
-            // check if current elem is smaller than smallestVal
-            if (unsorted[i] < smallestVal) {
-                smallestVal = unsorted[i];
-                smallestIndex = i;
-            }
+/**
+ * Finds the smallest unsorted element and adds it to the end of a sorted list.
+ *
+ * Time Complexity in worst case is 0(n^2) since we have to iterate
+ * over each of the n elements in of the list to find the smallest unsorted elements
+ * and repeat this process n times as only one element gets sorted on each pass.
+ *
+ * Time complexity in best case is Ω(n^2) since there is no gurantee the list is sorted
+ * until we go through the process n times for each element (until we actually run selection sort).
+ *
+ * Space Complexity is constant at O(1) because we are pushing smallest element to sorted array
+ * which is a constant operation.
+ *
+ */
+
+// findSmallest :: Array -> Number
+const findSmallest = xs => {
+    // begin with first elem in list
+    let acc = xs[0];
+    // store smallest elem
+    let smallest;
+    for (let i = 0; i < xs.length; i += 1) {
+        if (acc < xs[i]) {
+            smallest = acc;
+        } else {
+            smallest = xs[i];
+            acc = smallest;
         }
-        // push smallest to sortedList
-        sortedList.push(smallestVal);
-        // delete smallest from unsorted list
-        unsorted.splice(smallestIndex, 1);
     }
-    return sortedList;
+    return smallest;
 };
 
-let list1 = [14, 13, 12, 10, 9];
+// selectionSort :: UnsortedArray -> SortedArray
+const selectionSort = xs => {
+    const sorted = [];
+    // as long as there are no unsorted values
+    while (xs.length !== 0) {
+        // find the smallest in unsorted
+        const smallest = findSmallest(xs);
+        // remove smallest from unsorted
+        const smallestIndex = xs.indexOf(smallest);
+        xs.splice(smallestIndex, 1);
+        // add smallest to sorted
+        sorted.push(smallest);
+    }
+    return sorted;
+};
 
-console.log(selectionSort(list1));
-
-let list2 = [5, 95, 60, 55, 29, 75, 27,	18,	56, 5, 73, 29,	79,	36,	34,
-             1,	87, 57, 58,	32];
-
-console.log(selectionSort(list2));
+module.exports = selectionSort;
