2f24234106167b7e08ae701da488c2cabfd1bee3
Khalid <khalid@alum.recurse.com>
2018-02-02 01:22:28 +0000

    Re-describe mergeSort test

--- src/__tests__/mergesort.test.js
+++ src/__tests__/mergesort.test.js
@@ -1,7 +1,7 @@
 const mergeSort = require("../mergesort");
 
-describe("merge sort", () => {
-    it("sorts unsorted array using the classic divide and conquer paradigm", () => {
+describe("mergeSort", () => {
+    it("recursively sorts unsorted list", () => {
         expect(mergeSort([1, 8, 2, 4, 3, 7, 9])).toEqual([1, 2, 3, 4, 7, 8, 9]);
         expect(mergeSort([5, 2, 1, 3, 6, 4])).toEqual([1, 2, 3, 4, 5, 6]);
     });
