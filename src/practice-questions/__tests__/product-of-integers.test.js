0851453d98db727ba17a2075d23d7ef175c54ece
Khalid <khalid@alum.recurse.com>
2018-02-13 16:18:53 +0000

    Include test for a greedy approach

--- src/practice-questions/__tests__/product-of-integers.test.js
+++ src/practice-questions/__tests__/product-of-integers.test.js
@@ -1,19 +1,35 @@
-const getProductsOfAllIntsExceptAtIndex = require("../product-of-integers");
+const p = require("../product-of-integers");
 
 describe("getProductsOfAllIntsExceptAtIndex", () => {
     it("produces the product of integers except the integer at that index", () => {
-        expect(getProductsOfAllIntsExceptAtIndex([1, 2, 3])).toEqual([6, 3, 2]);
-        expect(getProductsOfAllIntsExceptAtIndex([1, 2, 3, 4])).toEqual([
+        expect(p.getProductsOfAllIntsExceptAtIndex([1, 2, 3])).toEqual([
+            6,
+            3,
+            2
+        ]);
+        expect(p.getProductsOfAllIntsExceptAtIndex([1, 2, 3, 4])).toEqual([
             24,
             12,
             8,
             6
         ]);
-        expect(getProductsOfAllIntsExceptAtIndex([1, 7, 3, 4])).toEqual([
+        expect(p.getProductsOfAllIntsExceptAtIndex([1, 7, 3, 4])).toEqual([
             84,
             12,
             28,
             21
+        ]);
+    });
+});
+
+describe("productOfIntsExceptAtIndex", () => {
+    it("produces product of integers using a greedy approach", () => {
+        expect(p.productOfIntsExceptAtIndex([1, 2, 3, 4, 5])).toEqual([
+            120,
+            60,
+            40,
+            30,
+            24
         ]);
     });
 });
