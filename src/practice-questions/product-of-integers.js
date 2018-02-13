58167238d472fff1d9fc7a7abc6c67e527fba60a
Khalid <khalid@alum.recurse.com>
2018-02-13 16:51:31 +0000

    Tidy up and re-implement using a greedy algorithm

--- src/practice-questions/product-of-integers.js
+++ src/practice-questions/product-of-integers.js
@@ -1,11 +1,14 @@
-//  You have an array of integers, and for each index you want to find the product of every integer // except the integer at that index.
+// You have an array of integers, and for each index you want to find the product
+// of every integer except the integer at that index.
 
 /**
  * getProductsOfAllIntsExceptAtIndex :: [Int] -> [Int]
  *
  * produces the product of integers except the integer at that index
  */
-const productOfIntsExceptAtIndex = xs => {
+
+// brute force solution
+const getProductsOfAllIntsExceptAtIndex = xs => {
     let result = [];
     const highIndex = xs.length - 1;
     let acc;
@@ -21,4 +24,30 @@ const productOfIntsExceptAtIndex = xs => {
     return result;
 };
 
-module.exports = productOfIntsExceptAtIndex;
+// the run time for the brute-force solution is O(n^2) can we do better?
+// yes, we can use a greedy approach
+// we can store each product so far and then multiply by the next integer
+const productOfIntsExceptAtIndex = xs => {
+    const result = [];
+    let product = 1;
+    if (xs.length < 2) {
+        throw "the size of input is too small";
+    }
+    // get products of ints before the int at index
+    for (let i = 0; i < xs.length; i++) {
+        result.push(product);
+        product *= xs[i];
+    }
+    product = 1;
+    // get products of ints after the int at index
+    for (let j = xs.length - 1; j >= 0; j--) {
+        result[j] *= product;
+        product *= xs[j];
+    }
+    return result;
+};
+
+module.exports = {
+    getProductsOfAllIntsExceptAtIndex: getProductsOfAllIntsExceptAtIndex,
+    productOfIntsExceptAtIndex: productOfIntsExceptAtIndex
+};
