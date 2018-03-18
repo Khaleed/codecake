e473d56c255310768bcdfa5b55529b29bd4b5488
Khalid <khalid@alum.recurse.com>
2018-03-18 17:26:13 +0000

    Remove comments, rename function, and add more robust tests

--- src/practice-questions/highest_product_three.py
+++ src/practice-questions/highest_product_three.py
@@ -1,42 +1,41 @@
 """
 
-Given an array ints find the 3 ints
+Given an a list of ints find the 3 ints
 with the highest product.
 
-Constraints: 1. list can't be less than 3 (Fail Fast)
-            2. if 3 then multiply those 3 ints
-            3. if 4 ints ex: [2, 4, 1, 5] --> 2 * 4 * 5
-            4. 4 ints [2, -5, 3, 6] --> 2 * 3 * 6
-            5. 4 ints [3, -5, 2, -6] --> 3 * -5 * -6
-
->>> find_highest_three([2, 5, 4])
+>>> highest_three_product([2, 5, 4])
 40
 
->>> find_highest_three([2, 4, 1, 5])
+>>> highest_three_product([2, 4, 1, 5])
 40
 
->>> find_highest_three([2, -5, 3, 6])
+>>> highest_three_product([2, -5, 3, 6])
 36
 
->>> find_highest_three([3, -5, 2, -6])
+>>> highest_three_product([3, -5, 2, -6])
 90
+
+>>> highest_three_product([3, 11, 2, -4, 6, -3, 7, 8, -7])
+616
+
+>>> highest_three_product([3, -11, 2, -4, 6, -3, 7, -8, -7])
+616
 
 """
 
-
-# find_highest_three :: [Int] --> Int
+# highest_three_product :: [Int] -> Int
 # Find the highest product of 3 ints in a given list
-def find_highest_three(xs):
+def highest_three_product(xs):
 
     if len(xs) < 3:
         print("List must have at least three integers")
 
     # initial state
-    largestProductOf3 = xs[0] * xs[1] * xs[2]  # 3 * -5 * 2 = -30
-    largestProductOf2 = xs[0] * xs[1]  # 3 * - 5 = -15
-    smallestProductOf2 = xs[0] * xs[1]  # 3 * -5 = -15
-    largest = max(xs[0], xs[1])  # 3, - 5 --> 3
-    smallest = min(xs[0], xs[1])  # 3,- 5 --> -5
+    largestProductOf3 = xs[0] * xs[1] * xs[2]
+    largestProductOf2 = xs[0] * xs[1]
+    smallestProductOf2 = xs[0] * xs[1]
+    largest = max(xs[0], xs[1])
+    smallest = min(xs[0], xs[1])
 
     for i in range(2, len(xs)):
         # do we have a new largestProductOf3?
